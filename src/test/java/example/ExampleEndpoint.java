b0636834931b9b77f01a583b86c63c1a4a0c8250
Bram Buitendijk <bram.buitendijk@huygens.knaw.nl>
2016-04-28 11:16:53 +0200

    Add test for request with queryParameters

--- src/test/java/example/ExampleEndpoint.java
+++ src/test/java/example/ExampleEndpoint.java
@@ -4,9 +4,9 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import nl.knaw.huygens.Log;
 
 @Path("/example")
@@ -30,4 +30,12 @@ public class ExampleEndpoint {
   public Response helloWorldJson() {
     return Response.ok("{\"hello\": \"world\"}").build();
   }
+
+  @GET
+  @Path("query")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response returnQueryParams(@QueryParam("par1") String par1, @QueryParam("par2") String par2) {
+    return Response.ok("{\"par1\": \"" + par1 + "\",\"par2\": \"" + par2 + "\"}").build();
+  }
+
 }
